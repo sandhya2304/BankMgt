2bb873bb171eab4dff435f4901a5ee0af273fd52
ram <sandhyasharma0423@gmail.com>
2017-07-26 12:15:23 +0530

    comit chnage in student
    Change-Id: I64fb4e4bc6beaf99520cba28c519809d4944c94a

--- src/pack/Student.java
+++ src/pack/Student.java
@@ -3,9 +3,18 @@ package pack;
 public class Student
 {
 	public Student() 
-	{
-		
+	{	
 		System.out.println("hello student ");
+	} 
+	public void hello()
+	{
+		System.out.println("helop ");
+	}
+	
+	public static void main(String[] args)
+	{
+		Student s=new Student();
+		s.hello();
 	}
 
 }
